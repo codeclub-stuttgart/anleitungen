5cabc29674329f51610ca36aec95214446774233
Paul Sonnentag <paul.sonnentag@googlemail.com>
2016-12-14 20:39:36 +0100

    Add dialog recipe

---
+++ _instructions/dialog.md
@@ -0,0 +1,85 @@
+---
+layout: instruction
+title: "Mit Figuren reden"
+---
+
+## Figur sprechen lassen
+
+![](dialog/hello-cat.png)
+
+Dieser Block zeigt eine Sprechblase für 2 Sekunden lang an erst danach wird der Block darunter gestartet
+
+```blocks
+    sage [Hallo!] für (2) Sek.
+```
+
+Dieser Block zeigt eine Sprechblase und macht keine Pause  
+
+```blocks
+    sage [Hallo!]
+```
+
+Du musst die Sprechblase mit einem zweiten leeren Block wieder ausblenden
+
+```blocks
+    sage []
+```
+
+<div class="checklist-symbol">Checkliste</div>
+
+- Probiere einmal beide Blöcke aus
+- Schau einmal was der Unterschied zu den <span class="visual-block">denke</span>-Blöcken ist     
+
+
+<div class="test-symbol">Teste dein Projekt</div>
+
+## Mit Figur reden
+
+Eine Figur kann auch Fragen stellen
+
+<div class="checklist-symbol">Checklist</div>
+
+- Wähle eine Figur aus
+
+- Füge folgenden Block hinzu
+
+```blocks
+    Wenn die grüne Flagge angeklickt
+    frage [Soll ich die Schatztruhe öffnen?] und warte
+```
+
+<div class="test-symbol">Teste dein Projekt</div>
+
+Es wird jetzt ein Eingabefeld angezeigt. Der Computer merkt sich dann was du eingibst. 
+
+![](dialog/answer-input.png)
+
+Um auf die Antwort zu reagieren verwende wir im nächsten Schritt den <span class="sensor-value">Antwort</span>-Block 
+
+- Füge unter dem <span class="sensor-block">frage</span>-Block diese Blöcke ein 
+
+```blocks
+    falls <(Antwort) = [ja]> dann
+        sage [Toll, ganz viel Gold] für (2) Sek.
+    sonst
+        sage [Dann eben nicht] für (2) Sek. 
+    Ende
+```
+
+<div class="test-symbol">Teste dein Projekt</div>
+
+-  Du kannst auch zwischen mehreren Antworten unterscheiden. Füge dazu unter sonst noch einen <span class="control-block">falls</span>-Block ein  
+
+
+```blocks
+    falls <(Antwort) = [ja]> dann
+        sage [Toll, ganz viel Gold] für (2) Sek.
+    sonst
+        falls <(Antwort) = [nein]> dann
+            sage [Dann eben nicht] für (2) Sek.
+        sonst
+            sage [Ich weiß nicht was du meinst] für (2) Sek.
+    Ende
+```
+
+<div class="test-symbol">Teste dein Projekt</div>
---
+++ vendor/language-de.js
@@ -0,0 +1,35 @@
+scratchblocks2._translations = {
+  "de": {
+    "palette": {
+      "Sound": "Klang",
+      "Control": "Steuerung",
+      "More Blocks": "Weitere Blöcke",
+      "Sensing": "Fühlen",
+      "variables": "Variablen",
+      "Operators": "Operatoren",
+      "list": "Liste",
+      "lists": null,
+      "Motion": "Bewegung",
+      "Pen": "Malstift",
+      "Looks": "Aussehen",
+      "variable": "Variable",
+      "Tips": "Tipps",
+      "Data": "Daten",
+      "Events": "Ereignisse"
+    },
+    "code": "de",
+    "blocks": ["gehe _ er-Schritt", "drehe dich @ um _ Grad", "drehe dich @ um _ Grad", "setze Richtung auf _", "drehe dich zu _", "gehe zu x:_ y:_", "gehe zu _", "gleite in _ Sek. zu x:_ y:_", "ändere x um _", "setze x auf _", "ändere y um _", "setze y auf _", "pralle vom Rand ab", "setze Drehtyp auf _", "x-Position", "y-Position", "Richtung", "sage _ für _ Sek.", "sage _", "denke _ für _ Sek.", "denke _", "zeige dich", "verstecke dich", "wechsle zu Kostüm _", "nächstes Kostüm", "wechsle zu Bühnenbild _", "ändere _-Effekt um _", "setze _-Effekt auf _", "schalte Grafikeffekte aus", "ändere Größe um _", "setze Größe auf _%", "komme nach vorn", "gehe _ Ebenen nach hinten", "Kostümnummer", "Bühnenbildname", "Größe", "wechsle zu Bühnenbild _ und warte", "nächstes Bühnenbild", "Bühnenbildnummer", "ziehe Kostüm _ an", "wechsle zum Hintergrund _", "nächster Hintergrund", "Hintergrund Nr.", "spiele Klang _", "spiele Klang _ ganz", "stoppe alle Klänge", "spiele Schlaginstrument _ für _ Schläge", "pausiere _ Schläge", "spiele Ton _ für _ Schläge", "setze Instrument auf _", "ändere Lautstärke um _", "setze Lautstärke auf _%", "Lautstärke", "ändere Tempo um _", "setze Tempo auf _ Schläge/Min.", "Tempo", "wische Malspuren weg", "hinterlasse Abdruck", "schalte Stift ein", "schalte Stift aus", "setze Stiftfarbe auf _", "ändere Stiftfarbe um _", "setze Stiftfarbe auf _", "ändere Farbstärke um _", "setze Farbstärke auf _", "ändere Stiftdicke um _", "setze Stiftdicke auf _", "setze _ auf _", "ändere _ um _", "zeige Variable _", "verstecke Variable _", "füge _ zu _ hinzu", "lösche _ aus _", "füge _ als _ in _ ein", "ersetze Element _  von _ durch _", "Element _ von _", "Länge von _", "_ enthält _", "zeige Liste _", "verstecke Liste _", "Wenn @ angeklickt", "Wenn Taste _ gedrückt", "Wenn ich angeklickt werde", "", "Wenn das Bühnenbild zu _ wechselt", "Wenn _ > _", "Wenn ich _ empfange", "sende _ an alle", "sende _ an alle und warte", "warte _ Sek.", "wiederhole _ mal", "wiederhole fortlaufend", "falls _ dann", "sonst", "Ende", "warte bis _", "wiederhole bis _", "stoppe _", "Wenn ich als Klon entstehe", "erzeuge Klon von _", "lösche diesen Klon", "falls _", "wiederhole fortlaufend, falls _", "stoppe dieses Skript", "stoppe alles", "wird _ berührt?", "wird Farbe _ berührt?", "Farbe _ berührt _?", "Entfernung von _", "frage _ und warte", "Antwort", "Taste _ gedrückt?", "Maustaste gedrückt?", "Maus x-Position", "Maus y-Position", "Lautstärke", "Video _ auf _", "schalte Video _", "setze Video-Transparenz auf _%", "Stoppuhr", "setze Stoppuhr zurück", "_ von _", "_ im Moment", "Tage seit 2000", "Benutzername", "laut?", "", "", "", "", "Zufallszahl von _ bis _", "", "", "", "_ und _", "_ oder _", "nicht _", "verbinde _ _", "Zeichen _ von _", "Länge von _", "_ mod _", "_ gerundet", "_ von _", "Wenn _", "Wenn _ _ _", "Sensor _?", "Wert von Sensor _", "schalte _ für _ Sekunden an", "schalte _ an", "schalte _ aus", "", "", "Wenn Abstand _ _", "Wenn Neigung _ _", "Abstand", "Neigung", "schalte Motor für _ Sekunden an", "schalte Motor an", "schalte Motor aus", "setze Motorleistung auf _", "setze Motordrehung _", "Wenn Abstand < _", "Wenn Neigung = _", "schalte Motor an", "schalte Motor aus", "schalte Motor für _ Sekunden an", "setze Motorkraft auf _", "setze Motorrichtung auf _", "", ""],
+    "osis": ["andere Skripte der Figur"],
+    "ignorelt": ["Wenn Abstand"],
+    "define": ["Definiere"],
+    "math": ["Betrag", "abrunden", "aufrunden", "Wurzel", "sin", "cos", "tan", "asin", "acos", "atan", "ln", "log", "e^", "10^"],
+    "aliases": {
+      "Wenn ⚑ angeklickt": "when @green-flag clicked",
+      "drehe dich ↻ um _ Grad": "turn @arrow-cw _ degrees",
+      "Wenn die grüne Flagge angeklickt": "when @green-flag clicked",
+      "drehe dich ↺ um _ Grad": "turn @arrow-ccw _ degrees",
+      "drehe dich nach links um _ Grad": "turn @arrow-ccw _ degrees",
+      "drehe dich nach rechts um _ Grad": "turn @arrow-cw _ degrees"
+    }
+  }
+};
